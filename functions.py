ea6c9a4e88de3e965df11e90f71566c26b20b6ed
adityaahuja7 <aditya20275@iiitd.ac.in>
2024-04-09 19:46:50 +0530

    Add Attention Branch Class model.

--- functions.py
+++ functions.py
@@ -6,25 +6,27 @@ import tensorflow as tf
 import scipy.fftpack
 import hyperparameters as hp
 
-F = hp.F # number of LFCC features
-T = hp.T # number of frames
-
+F = hp.F  
+T = hp.T  
 
 def read_flac_file(flac_file_path):
     signal, sample_rate = sf.read(flac_file_path)
     return signal, sample_rate
 
-def extract_lfcc(signal, sample_rate, n_lfcc=F, target_frames=T):
-    stft = np.abs(librosa.stft(signal, n_fft=2048))
+
+def extract_lfcc(signal, sample_rate, n_lfcc= hp.F, target_frames= hp.T):
+    stft = np.abs(librosa.stft(signal, n_fft= hp.n_fft, hop_length= hp.hop_length))
     log_spectrogram = librosa.amplitude_to_db(stft)
     lfcc = scipy.fftpack.dct(log_spectrogram, axis=0, type=2, norm='ortho')[:n_lfcc]
+    
+    # Ensure the LFCC is shaped as (n_lfcc, target_frames)
     if lfcc.shape[1] > target_frames:
         lfcc = lfcc[:, :target_frames]
     else:
-        lfcc = np.pad(lfcc, ((0, 0), (0, target_frames - lfcc.shape[1])), mode='constant')
-    return lfcc.T
+        lfcc = np.pad(lfcc, ((0, 0), (0, target_frames - lfcc.shape[1])), 'constant')
+    
+    # Explicitly reshape to ensure the output is (1, n_lfcc, target_frames)
+    # This step is crucial to avoid adding an unintended dimension
+    lfcc = lfcc.reshape(1, n_lfcc, target_frames)
 
-
-
-
-
+    return lfcc
--- hyperparameters.py
+++ hyperparameters.py
@@ -1,2 +1,9 @@
-T = 50
-F = 50
+T = 300 # n_frames
+F = 10 # n_lfcc
+n_epochs_training = 1
+
+# 1xFxT 
+
+#HYPERPARAMETERS FOR LFCC
+n_fft = 2048
+hop_length = 512
